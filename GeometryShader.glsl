abe11668db1c45aa116d24b19a58f195766aa158
Jennifer L-S <jenny-katt@hotmail.com>
2019-01-17 22:01:30 +0100

    Normals now go though geometry shader also.

--- GeometryShader.glsl
+++ GeometryShader.glsl
@@ -7,13 +7,16 @@ uniform mat4 VIEW_MAT;
 uniform mat4 MODEL_MAT;
 
 in vec2 aTexture[];
+in vec3 normalsOut[];
 out vec2 texUVs;
 out vec4 fragPos;
+out vec3 finalNormals;
 
 void main(){
 	//Input primitive
 	for(int i = 0; i < gl_in.length(); i++){
 		texUVs = aTexture[i];
+		finalNormals = normalsOut[i];
 		gl_Position = gl_in[i].gl_Position * MODEL_MAT * VIEW_MAT * PROJ_MAT; //Reversed order
 		fragPos = MODEL_MAT * gl_in[i].gl_Position; //Position in world space
 		EmitVertex();
