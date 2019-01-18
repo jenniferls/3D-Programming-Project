58de1f9180e0299efc68bb6454ec7d9e902bd362
PAULINA FRISK <pafr17@student.bth.se>
2019-01-18 16:49:48 +0100

    Ambient and diffuse light added.

--- Fragment.glsl
+++ Fragment.glsl
@@ -6,10 +6,29 @@ layout(location=3) in vec3 finalNormals;
 // this is the final pixel colour
 out vec4 fragment_color;
 
+// PF: the diffuse value
+in float diffValue;
+
 // this is a uniform value, the very same value for ALL pixel shader executions
 uniform sampler2D textureSampler;
+
 void main () {
-	fragment_color = texture(textureSampler, texUVs);
+	
+	float amStr = 0.1f; // PF: the ambient strenght 
+	vec3 am = amStr * vec3(1.0, 1.0, 1.0); // PF: the ambient 
+
+	//vec4 texSample = texture( textureSampler, vec2( 1 - texUVs.s, 1 - texUVs.t)) * diffValue;
+	//vec4 applyAm = texture( textureSampler, vec2( 1 - texUVs.s, 1 - texUVs.t)) * vec4(am, 1.0f);
+
+	vec4 texSample = texture( textureSampler, texUVs) * diffValue;
+	vec4 applyAm = texture(textureSampler, texUVs) * vec4(am, 1.0f);
+
+
+	fragment_color = applyAm; 
+	fragment_color += texSample;
+
+
+	//fragment_color = texture(textureSampler, texUVs);
 
 	//fragment_color = vec4 (color * colourFromImGui, 1.0);
 	//fragment_color = myAttrOut * vec4 (color * colourFromImGui, 1.0);
--- GeometryShader.glsl
+++ GeometryShader.glsl
@@ -14,11 +14,37 @@ out vec2 texUVs;
 out vec4 fragPos;
 out vec3 finalNormals;
 
+out float diffValue;
+const vec3 light_pos = vec3( 0.0, 0.0, 2.0);
+//in vec4 vertexPos;
+
+float getDiffVal(vec4 normal){
+	
+	vec4 normalVec = normalize( vec4(light_pos, 1.0));
+	float diffuseFactor = dot(normalVec.xyz, normal.xyz); 
+
+	if (diffuseFactor < 0)
+		diffuseFactor = 0;
+	
+	return diffuseFactor * 4.0f * (1.0/(length(vec4(light_pos, 1.0))));
+}
+
+//vec4 getNormal(){
+//
+//	vec3 u = vec3(vertexPos[0]) - vec3(vertexPos[1]);
+//	vec3 v = vec3(vertexPos[2]) - vec3(vertexPos[1]);
+//
+//	return vec4(normalize(cross(u,v)), 0.0f);
+//}
+//
+
 void main(){
 	//First determine the normal of the face by calculating the cross product of two vectors created by 3 triangle vertices
 	//This gives us a vector that is perpendicular to the two vectors
 	vec3 n = cross(gl_in[2].gl_Position.xyz - gl_in[0].gl_Position.xyz, gl_in[1].gl_Position.xyz - gl_in[0].gl_Position.xyz); //Order determines if the resulting vector will be positive or negative.
 	vec3 faceNormal = mat3(VIEW_MAT * MODEL_MAT) * n; //Make sure the normal is in view space
+
+	vec4 theNormal = vec4( n, 1.0f) * MODEL_MAT;
 
 	//Display triangle if it's facing the camera
 	float angle = dot(faceNormal, vec3((PROJ_MAT * VIEW_MAT * MODEL_MAT) * gl_in[0].gl_Position));
@@ -30,6 +56,7 @@ void main(){
 			//gl_Position = gl_in[i].gl_Position * (MODEL_MAT * VIEW_MAT * PROJ_MAT); //Reversed order (works if matrices aren't transposed)
 			//gl_Position = MVP_MAT * gl_in[i].gl_Position; //Test
 			fragPos = MODEL_MAT * gl_in[i].gl_Position; //Position in world space
+			diffValue = getDiffVal( theNormal);
 			EmitVertex();
 		}
 		EndPrimitive();
