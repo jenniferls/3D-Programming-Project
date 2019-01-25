b7ee61ff25a48cd81ad6480fd1fa29a86efa44f6
Jennifer L-S <jenny-katt@hotmail.com>
2019-01-25 11:56:41 +0100

    Started working on obj-file loader

--- OBJLoader.h
+++ OBJLoader.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <vector>
+#include "glm/glm.hpp"
 
 class OBJLoader {
 public:
 	OBJLoader();
 	~OBJLoader();
 
-	void loadOBJ(const char* filename);
+	void loadOBJ(const char* filename, std::vector<glm::vec3> &verts, std::vector<glm::vec3> &normals, std::vector<glm::vec2> &uvs, int &elements);
 
 private:
 
