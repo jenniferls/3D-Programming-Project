6b189f78a3fe9c8a3fcf969e8edd04f82f917b6f
Jennifer L-S <jenny-katt@hotmail.com>
2019-01-30 15:20:47 +0100

    Changed diffuse calculations from geometry-shader to fragment-shader with noticable results.

--- Fragment.glsl
+++ Fragment.glsl
@@ -13,6 +13,10 @@ in float specValue;
 // this is a uniform value, the very same value for ALL pixel shader executions
 uniform sampler2D textureSampler;
 
+const vec3 light_pos = vec3( 4.0, 0.0, 2.0);
+in vec4 fragPos;
+uniform mat4 MODEL_MAT;
+
 void main () {	
 	float amStr = 0.5f; // PF: the ambient strenght 
 	float specStr = 0.5f; // PF: the specular strenght
@@ -20,11 +24,18 @@ void main () {
 	float MatSpecIntensity = 1.0f;
 
 	vec3 am = amStr * lightColor; // PF: the ambient 
-	vec3 diffuse = diffValue * lightColor; // PF: the diffuse
+//	vec3 diffuse = diffValue * lightColor; // PF: the diffuse
 	vec3 spec = specStr * specValue * lightColor;
 	
 	vec4 texSample = texture( textureSampler, vec2(texUVs.s, 1 - texUVs.t));
 
-	vec4 result = vec4(am + diffuse + spec, 1.0 ) * texSample;
+	vec3 pointToLight = normalize(light_pos - fragPos.xyz);
+	vec3 norm = normalize(mat3(MODEL_MAT) * finalNormals); //Make sure the vectors are normalized in world space
+	float diffuseFactor = dot(pointToLight, norm) / (length(pointToLight) * length(norm));
+	diffuseFactor = clamp(diffuseFactor, 0, 1); //Make sure the diffuse factor isn't negative or above 1
+	vec3 diffuse = diffuseFactor * lightColor;
+
+
+	vec4 result = vec4(am + diffuse, 1.0 ) * texSample;
 	fragment_color = result;
 }
