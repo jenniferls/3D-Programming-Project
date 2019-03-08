c4bdf35eeaf5ac3c8b4a3f599d8a9999d2016e1a
Jennifer L-S <jenny-katt@hotmail.com>
2019-03-08 15:47:43 +0100

    Added blendmap shaders (no work started on blendmapping yet)

---
+++ GeometryShaderBlend.glsl
@@ -0,0 +1,7 @@
+#version 440
+layout(triangles) in;
+layout(triangle_strip, max_vertices = 3) out;
+
+void main(){
+	
+}
--- main.cpp
+++ main.cpp
@@ -60,6 +60,9 @@ GLuint gShaderProgramPS = 0;
 //Skybox shader
 GLuint gShaderProgramSkybox = 0;
 
+//Blend-mapping shader
+GLuint gShaderProgramBlend = 0;
+
 // full screen quad stuff
 GLuint gVertexBufferFS = 0;
 GLuint gVertexAttributeFS = 0;
@@ -209,6 +212,104 @@ void CreateSMShaders() {
 	glDetachShader(gShaderProgramSM, fs);
 	glDeleteShader(vs);
 	glDeleteShader(fs);
+}
+
+void CreateBlendmapShaders() {
+	// local buffer to store error strings when compiling.
+	char buff[1024];
+	memset(buff, 0, 1024);
+	GLint compileResult = 0;
+
+	//create vertex shader "name" and store it in "vs"
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+
+	// open .glsl file and put it in a string
+	ifstream shaderFile("VertexShaderBlend.glsl");
+	std::string shaderText((std::istreambuf_iterator<char>(shaderFile)), std::istreambuf_iterator<char>());
+	shaderFile.close();
+
+	// glShaderSource requires a double pointer.
+	// get the pointer to the c style string stored in the string object.
+	const char* shaderTextPtr = shaderText.c_str();
+
+	// ask GL to use this string a shader code source
+	glShaderSource(vs, 1, &shaderTextPtr, nullptr);
+
+	// try to compile this shader source.
+	glCompileShader(vs);
+
+	// check for compilation error
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &compileResult);
+	if (compileResult == GL_FALSE) {
+		// query information about the compilation (nothing if compilation went fine!)
+		glGetShaderInfoLog(vs, 1024, nullptr, buff);
+		// print to Visual Studio debug console output
+		OutputDebugStringA(buff);
+	}
+
+	// repeat process for Fragment Shader (or Pixel Shader)
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	shaderFile.open("FragmentShaderBlend.glsl");
+	shaderText.assign((std::istreambuf_iterator<char>(shaderFile)), std::istreambuf_iterator<char>());
+	shaderFile.close();
+	shaderTextPtr = shaderText.c_str();
+	glShaderSource(fs, 1, &shaderTextPtr, nullptr);
+	glCompileShader(fs);
+	// query information about the compilation (nothing if compilation went fine!)
+	compileResult = GL_FALSE;
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &compileResult);
+	if (compileResult == GL_FALSE) {
+		// query information about the compilation (nothing if compilation went fine!)
+		memset(buff, 0, 1024);
+		glGetShaderInfoLog(fs, 1024, nullptr, buff);
+		// print to Visual Studio debug console output
+		OutputDebugStringA(buff);
+	}
+
+	// repeat process for Geometry Shader
+	GLuint gs = glCreateShader(GL_GEOMETRY_SHADER);
+	shaderFile.open("GeometryShaderBlend.glsl");
+	shaderText.assign((std::istreambuf_iterator<char>(shaderFile)), std::istreambuf_iterator<char>());
+	shaderFile.close();
+	shaderTextPtr = shaderText.c_str();
+	glShaderSource(gs, 1, &shaderTextPtr, nullptr);
+	glCompileShader(gs);
+	// query information about the compilation (nothing if compilation went fine!)
+	compileResult = GL_FALSE;
+	glGetShaderiv(gs, GL_COMPILE_STATUS, &compileResult);
+	if (compileResult == GL_FALSE) {
+		// query information about the compilation (nothing if compilation went fine!)
+		memset(buff, 0, 1024);
+		glGetShaderInfoLog(gs, 1024, nullptr, buff);
+		// print to Visual Studio debug console output
+		OutputDebugStringA(buff);
+	}
+
+	//link shader program (connect vs, gs and ps)
+	gShaderProgramBlend = glCreateProgram();
+	glAttachShader(gShaderProgramBlend, fs);
+	glAttachShader(gShaderProgramBlend, vs);
+	glAttachShader(gShaderProgramBlend, gs);
+	glLinkProgram(gShaderProgramBlend);
+
+	// check once more, if the Vertex Shader, Geometry Shader and the Fragment Shader can be used together
+	compileResult = GL_FALSE;
+	glGetProgramiv(gShaderProgramBlend, GL_LINK_STATUS, &compileResult);
+	if (compileResult == GL_FALSE) {
+		// query information about the compilation (nothing if compilation went fine!)
+		memset(buff, 0, 1024);
+		glGetProgramInfoLog(gShaderProgramBlend, 1024, nullptr, buff);
+		// print to Visual Studio debug console output
+		OutputDebugStringA(buff);
+	}
+	// in any case (compile sucess or not), we only want to keep the 
+	// Program around, not the shaders.
+	glDetachShader(gShaderProgramBlend, vs);
+	glDetachShader(gShaderProgramBlend, fs);
+	glDetachShader(gShaderProgramBlend, gs);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+	glDeleteShader(gs);
 }
 
 void CreateFSShaders() {
